5c2ab94f9f641c976914462438233f2362bcf631
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-06-04 09:00:34 -0400

    sub category component

--- app/components/Subcategory.tsx
+++ app/components/Subcategory.tsx
@@ -2,14 +2,39 @@
 
 import { IconType } from "react-icons";
 
+import axios from "axios";
+
+import { toast } from "react-hot-toast";
+import { Reminder } from "@prisma/client";
+import { Priority } from "@prisma/client";
+
 interface SubcategoryProps {
   icon: IconType;
   label: string;
 }
 
 const Subcategory: React.FC<SubcategoryProps> = ({ icon: Icon, label }) => {
+  const handleClick = async () => {
+    const response = await axios.post<Reminder>("/api/reminders", {
+      title: "handle Atmaja",
+      description: "shes throwing a tantrum",
+      recurring: true,
+      priority: Priority.HIGH,
+      recurringFreq: 1,
+    });
+
+    if (response.status === 200) {
+      toast.success("reminder added!");
+    } else {
+      toast.error("error adding reminder!");
+    }
+  };
+
   return (
-    <div className="inline-flex gap-x-2 p-2">
+    <div
+      className="inline-flex gap-x-2 p-2 cursor-pointer "
+      onClick={handleClick}
+    >
       <Icon size={24} className="text-white" />
       <p className="w-full text-md font-md hover:text-white transition text-zinc-400">
         {label}
