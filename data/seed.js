78566c18bbca9889be8da1f40ed2caf1fcdb7a99
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-06-04 09:00:18 -0400

    added seed

---
+++ data/seed.js
@@ -0,0 +1,20 @@
+[
+  {
+    reminders: [
+      {
+        title: "handle Atmaja",
+        description: "shes throwing a tantrum",
+        recurring: true,
+        priority: HIGH,
+        recurringFreq: 1,
+      },
+    ],
+    users: [
+      {
+        name: "Mateusz Zukowski",
+        email: "matt@gmail.com",
+        emailVerified: false,
+      },
+    ],
+  },
+];
