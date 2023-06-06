08cdc87caf63671456353b1418615605104ee969
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-06-06 17:07:39 -0400

    added reminder POST

--- app/api/reminders/route.ts
+++ app/api/reminders/route.ts
@@ -1,15 +1,31 @@
 import { NextResponse } from "next/server";
 
 import prisma from "../../lib/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return null;
+  }
+
+  console.log(currentUser);
+
   const body = await request.json();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: currentUser.email as string,
+    },
+  });
 
   const reminder = await prisma.reminder.create({
     data: {
+      userId: user?.id,
       ...body,
     },
   });
 
-  return NextResponse.json(reminder);
+  return NextResponse.json(currentUser);
 }
