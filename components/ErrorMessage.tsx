fde66d39d95aff048f731f9ee55d608e5cfc97a6
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-06-06 17:08:14 -0400

    error message edit

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -2,17 +2,10 @@
 
 interface ErrorProps {
   message?: string;
-  type?: string;
 }
 
-const ErrorMessage: React.FC<ErrorProps> = ({ message, type }) => {
-  return (
-    <div className="w-full p-2 bg-rose-500 rounded-md ">
-      {type === "password"
-        ? `      Must contain ${message?.toLowerCase()}`
-        : `${message}`}
-    </div>
-  );
+const ErrorMessage: React.FC<ErrorProps> = ({ message }) => {
+  return <div className="w-full p-2 bg-rose-500 rounded-md ">{message}</div>;
 };
 
 export default ErrorMessage;
