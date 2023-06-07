333c902083bd0f81cff6353b17e39747d73924ce
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-06-07 17:28:55 -0400

    tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,10 +23,20 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        slide: {
+          from: { opacity: 1 },
+          to: { display: "none", transform: "translateX(-1000px)" },
+        },
+        enlarge: {
+          "0%, 100%": { background: "#082f49" },
+          "50%": { background: "#075985" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        slide: "slide 1s ease-out",
+        enlarge: "enlarge 2s ease-in-out infinite ",
       },
     },
   },
