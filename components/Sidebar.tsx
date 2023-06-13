8dc64a96a45e860e293b3bc60ae3caf465b83fbe
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-06-12 22:26:09 -0400

    added skeleton

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,26 +4,58 @@ import { Reminder } from "@/app/types/types";
 
 import { User } from "@prisma/client";
 import { IChildren } from "../app/types/types";
-import { FaUserCheck, FaUserPlus } from "react-icons/fa";
 import { SiClockify } from "react-icons/si";
 import { MdLogout } from "react-icons/md";
 
 import Subcategory from "./Subcategory";
-import Modal from "./Modal";
 import { signOut } from "next-auth/react";
 import LoginForm from "./LoginForm";
 import { useEffect, useState } from "react";
 import ReminderList from "./ReminderList";
 import CreateReminder from "./CreateReminder";
+import { Skeleton } from "./ui/skeleton";
 
-const Sidebar: React.FC<
-  IChildren & { currentUser: User | null; reminders: Reminder[] | null }
-> = ({ children, currentUser, reminders }) => {
+const Sidebar: React.FC<IChildren & { currentUser: User | null }> = ({
+  children,
+  currentUser,
+}) => {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     setMounted(true);
   }, []);
+
+  if (!mounted) {
+    return (
+      <div className="flex flex-row h-full divide-x divide-cyan-800">
+        <div className="w-[250px] hidden md:flex flex-col p-2">
+          <div className="flex flex-col gap-y-1 w-full divide-y divide-cyan-800">
+            <Subcategory
+              label="Priority Planner"
+              icon={SiClockify}
+              className="bg-lightbackground py-[43px] text-[20px]"
+              inverted
+            />
+            <div>
+              <div className="flex items-center mt-3 mb-2 w-full gap-2">
+                <Skeleton className="h-12 w-12 rounded-full bg-lightbackground" />
+                <div className="space-y-2 flex-1">
+                  <Skeleton className="h-4 w-full bg-lightbackground" />
+                  <Skeleton className="h-4 w-full bg-lightbackground" />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <Skeleton className="flex flex-row gap-3 items-center w-full px-5 rounded-md text-md py-2 my-2 group bg-lightbackground" />
+            </div>
+            <Skeleton className="flex flex-row gap-3 items-center w-full px-5 rounded-md text-md py-2 my-2 group bg-lightbackground" />
+          </div>
+        </div>
+        <main className="h-full flex-1 bg-background ">{children}</main>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-row h-full divide-x divide-cyan-800">
