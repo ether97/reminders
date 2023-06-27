21371f6d8641c592f79fea539db89c5ccf7001f2
Mateusz Zukowki <109141931+ether97@users.noreply.github.com>
2023-06-26 22:16:42 -0400

    testing s3

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,7 +2,11 @@ import { NextResponse } from "next/server";
 
 import prisma from "../../lib/prismadb";
 
+import pic1 from "../../assets/neom-sqG2iXx6f2k-unsplash.jpg";
+
 import bcrypt from "bcrypt";
+
+import AWS from "aws-sdk";
 
 export async function POST(request: Request) {
   const body = await request.json();
@@ -13,6 +17,24 @@ export async function POST(request: Request) {
   const salt = await bcrypt.genSalt(10);
 
   const hashedPassword = await bcrypt.hash(password, salt);
+
+  const credentials = {
+    accessKeyId: process.env.S3_ACCESS_KEY as string,
+    secretAccessKey: process.env.S3_SECRET_KEY as string,
+  };
+
+  AWS.config.update({ credentials: credentials, region: "us-east-1" });
+
+  const s3 = new AWS.S3();
+
+  var presignedPUTURL = s3.getSignedUrl("putObject", {
+    Bucket: "react-bucket1122",
+    Key: "test-file",
+    Body: pic1,
+    Expires: 10000,
+  });
+
+  console.log(presignedPUTURL);
 
   const user = await prisma.user.create({
     data: {
